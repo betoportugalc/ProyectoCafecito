52e0fc793f503900960d1d3bad767a4c5ed5e1a9
betoportugalc <alberto.portugal.c@tecsup.edu.pe>
2025-11-19 21:39:36 -0500

    Refactor despacho logic into a function

--- Diagrama 3: Modulo_de_despacho_y_entregas.py
+++ Diagrama 3: Modulo_de_despacho_y_entregas.py
@@ -1,30 +1,32 @@
-print("-"*40)
-print("MODULO DE DESPACHO Y ENTREGAS".center(40, "-"))
-print("-"*40)
-print("Pedido #01")
+def despacho_y_entrgas():
+    
+    print("-"*40)
+    print("MODULO DE DESPACHO Y ENTREGAS".center(40, "-"))
+    print("-"*40)
+    print("Pedido #01")
 
-pedido_correcto = input("el pedido correcto? (S/N): ").lower()
-if pedido_correcto=="s" :
-    entrega=input("Tipo de entrega:\n" \
+    pedido_correcto = input("el pedido correcto? (S/N): ").lower()
+    if pedido_correcto=="s" :
+        entrega=input("Tipo de entrega:\n" \
                    "1: Presencial\n" \
                    "2: Despacho/Delivery\n")
-    if entrega=="1" :
-        recojo=input("Notificar al mozo: Pedido Listo\n"
+        if entrega=="1" :
+            recojo=input("Notificar al mozo: Pedido Listo\n"
                            "Mozo recogio el pedido?(S/N)").lower()
-        if recojo=="s":
-            print("Pedido Entregado")
-        else:
-            print("Registrar incidencia")   
-    elif entrega=="2" :
-        recojo=input("Repartidor notidicado\n" 
+            if recojo=="s":
+                print("Pedido Entregado")
+            else:
+                print("Registrar incidencia")   
+        elif entrega=="2" :
+            recojo=input("Repartidor notidicado\n" 
                     "Repartidor recogio el pedido?(S/N)").lower()
-        if recojo=="s":
-            print("Pedido Entregado")
-        else:
-            print("Registrar incidencia")
+            if recojo=="s":
+                print("Pedido Entregado")
+            else:
+                print("Registrar incidencia")
 
-else :
-    print("Notificar a cocina: Pedido incorrecto")
+    else :
+        print("Notificar a cocina: Pedido incorrecto")
 
-print("\nFin del proceso de despacho y entregas")
-print("-"*40)
+    print("\nFin del proceso de despacho y entregas")
+    print("-"*40)
